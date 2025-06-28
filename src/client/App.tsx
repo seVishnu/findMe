ec69e5cfb605b6fa505cedd54de6476540e0f9d1
vishnu sekhar <vishnusekhar96@gmail.com>
2025-06-28 06:00:02 +0530

    feat: setup-runnable-client

---
+++ src/client/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+  return <h1>Hello from React Client 👋</h1>;
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/client/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist/client'),
+    filename: 'bundle.js',
+    clean: true
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ],
+  devServer: {
+    static: './dist/client',
+    port: 3000,
+    hot: true
+  },
+  mode: 'development'
+};
